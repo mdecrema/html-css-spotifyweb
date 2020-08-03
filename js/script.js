5866a883efa2f7ccc16cd19c1c3f1a158fbbc527
marco <marcodecrema@libero.it>
2020-08-03 17:35:16 +0200

    script js e commenti

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
-var brano = "Red Hot Chilli Peppers";
 
-document.getElementById('newTitle').innerHTML = brano;
+// nuova variabile contenente nome artisti
+var artisti = "Red Hot Chilli Peppers";
+
+// metodo per sostituire la 'var artisti' nella pagina HTML 
+document.getElementById('new-title').innerHTML = artisti;
